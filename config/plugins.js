bf50d44d8f6eaec01a76ef4cc62b32d74222d010
hieunv-0998 <nguyen.van.hieu@sun-asterisk.com>
2024-07-24 17:04:33 +0700

    feat: add all-in-one accessibility plugin

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,8 @@
-module.exports = () => ({});
+module.exports = () => ({
+  'all-in-one-accessibility': {
+    enabled: true,
+    resolve: './node_modules/strapi-plugin-all-in-one-accessibility',
+    config: {
+    },
+  },
+});
